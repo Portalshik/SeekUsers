0b585c1b4c27b3e1cc82cd687a39cdb36803c1cf
Portalshik <61150089+Portalshik@users.noreply.github.com>
2020-09-06 02:07:56 +0300

    Update up.php

--- up.php
+++ up.php
@@ -1,6 +1,6 @@
 <?php
-$seek = file_get_contents("https://github.com/Portalshik/SeekUsers/blob/master/seek_users.py");
-$jpg = file_get_contents("https://github.com/Portalshik/SeekUsers/blob/master/jpg.py");
+$seek = file_get_contents("https://raw.githubusercontent.com/Portalshik/SeekUsers/master/seek_users.py");
+$jpg = file_get_contents("https://raw.githubusercontent.com/Portalshik/SeekUsers/master/jpg.py");
 $file = fopen("seek_users.py", "w");
 fwrite($file, $seek);
 fclose($file);
